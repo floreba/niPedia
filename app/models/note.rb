3508fb4a688e91c65c6c572e5dbf8d7caf73aff2
Florian Bartel <bartel.florian@live.de>
2022-06-24 17:14:44 +0200

    add controller & routes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,5 @@
 class Note < ApplicationRecord
+  belongs_to :user
+  belongs_to :folder
+  has_many :taggings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_24_130948) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_24_143008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_130948) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "folder_id"
+    t.index ["folder_id"], name: "index_notes_on_folder_id"
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
@@ -55,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_130948) do
   end
 
   add_foreign_key "folders", "users"
+  add_foreign_key "notes", "folders"
   add_foreign_key "notes", "users"
   add_foreign_key "taggings", "notes", column: "reference_id"
   add_foreign_key "taggings", "notes", column: "tagger_id"
