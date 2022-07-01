67b5bc3719bcf929b086621a95659d956ed93977
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-07-01 19:41:00 +0200

    textfield autosave when changed

--- app/javascript/controllers/text_update_controller.js
+++ app/javascript/controllers/text_update_controller.js
@@ -2,22 +2,30 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="text-update"
 export default class extends Controller {
-  static targets = ["content"]
+  static targets = ["form"]
 
   connect() {
     const csrfToken = document.querySelector("meta[name=csrf-token]").getAttribute("content")
-    console.log("Connecting to text-update");
-    console.log(this.contentTarget.action);
+    // console.log("Connecting to text-update");
+    console.log(this.formTarget.action);
+    console.log(this.formTarget.action);
+  }
+
+  add() {
+    // console.log("Updating text-update");
+    fetch(this.formTarget.action, {
+      method: "PATCH",
+      headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
+      body: new FormData(this.formTarget)
+    })
   }
 
   update() {
-    console.log("Updating text-update");
-    fetch(this.contentTarget.action, {
+    // console.log("Updating text-update");
+    fetch(this.formTarget.action, {
       method: "PATCH",
       headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
-      body: new FormData(this.contentTarget)
+      body: new FormData(this.formTarget)
     })
-    .then(response => console.log(response))
-    console.log(this.contentTarget);
   }
 }
