023573d64dbb12f02c9b0ddd98504e73b60100b9
Florian Bartel <bartel.florian@live.de>
2022-07-03 00:44:03 +0200

    taggings controller 🙌🏻 finally

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -3,32 +3,6 @@ class TaggingsController < ApplicationController
     @taggings = policy_scope(Tagging.all)
   end
 
-  def new
-    @tagging = Tagging.new
-    # @note = Note.find(params[:id])
-    # raise
-  end
-
-  def create
-    raise
-    @tagging = Tagging.new(tagging_params)
-    @tagging.tagger_id = Note.find(params[:id])
-    @tagging.reference_id = policy_scope(Note).find_by('name = ?', @tagging.name)
-
-    if @tagging.reference_id.empty? || @tagging.reference.nill?
-      @reference_note = Note.new(name: @tagging.name)
-      @reference_note.save!
-      @tagging.reference_id = @reference_note.id
-    end
-    if @tagging.save!
-      redirect_to edit_note_path(@tagging.tagger_id), notice: 'Tagging was successfully created.'
-    else
-      render :index, status: :unprocessable_entity
-    end
-
-
-  end
-
   def destroy
     @tagging = Tagging.find(params[:id])
     authorize @tagging
@@ -38,9 +12,28 @@ class TaggingsController < ApplicationController
     # toilet_path(toilet), status: :see_other
   end
 
+  def create
+    @tagging = Tagging.new(tagging_params)
+
+    @tagging.reference = Note.find_by('name = ?', @tagging.name)
+    if @tagging.reference_id.nil?
+      # declare a note + current user & save
+      @reference_note = Note.new(name: @tagging.name)
+      @reference_note.user = current_user
+      @reference_note.save
+      @tagging.reference = @reference_note
+    end
+    authorize @tagging
+    if @tagging.save!
+      redirect_to edit_note_path(@tagging.tagger_id), notice: 'Tagging was successfully created.'
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def tagging_params
-    params.require(:tagging).permit(:name)
+    params.require(:tagging).permit(:name, :tagger_id)
   end
 end
--- app/policies/tagging_policy.rb
+++ app/policies/tagging_policy.rb
@@ -3,7 +3,15 @@ class TaggingPolicy < ApplicationPolicy
   def destroy?
     user
   end
-  
+
+  def create?
+    record.reference.user == user && record.tagger.user == user
+  end
+
+  # def create_tagging?
+  #   record.reference.user == user && record.tagger.user == user
+  # end
+
   class Scope < Scope
     def resolve
       scope.all
