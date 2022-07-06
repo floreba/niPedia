b878cfd526166d7a99c3c9c8eed0a95aa7db6749
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-07-06 16:28:40 +0200

    side bar transition when small screen fixed

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -8,6 +8,8 @@ export default class extends Controller {
     if(document.body.clientWidth > 600){
       console.log(this.sidebarDivTarget)
       this.sidebarDivTarget.classList.remove('close');
+      this.sidebarDivTarget.classList.add('transition');
+
     }else{
       this.sidebarDivTarget.classList.add('close');
     }
@@ -16,7 +18,6 @@ export default class extends Controller {
   toggleClose() {
     if(document.body.clientWidth > 600){
       this.sidebarDivTarget.classList.toggle('close');
-
     }else{
       this.sidebarDivTarget.classList.toggle('small-screen');
     }
